142fd2f2c503bdbd5b57d3788b3c0bb9342b25f2
Kintsuba <bnxth107@gmail.com>
2022-07-15 12:09:36 +0900

    fix: typo

--- src/functions/react.nim
+++ src/functions/react.nim
@@ -3,12 +3,11 @@ import ../types, ../misskey
 
 proc checkNote(note: Note, token: string): Future[bool] {.async.} =
   if note.text.contains("トレーラーちゃん"):
-    discard= await createReaction(token, note.id, "🚛")
+    discard await createReaction(token, note.id, "🚛")
     sleep(5000)
     return true
 
   return false
-    
 
 proc reactNotes*(notes: seq[Note], token: string): Future[seq[Note]] {.async.} =
   return notes.filterIt(await checkNote(it, token))
